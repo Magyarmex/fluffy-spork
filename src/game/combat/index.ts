f532444f3647b2f213c9078b2b1b87e57c829771
Magyarmex <magyarmex.github@gmail.com>
2026-08-11 02:12:07 -0600

    NOVASTAR Mission 10: export canonical combat API

---
+++ src/game/combat/index.ts
@@ -0,0 +1,17 @@
+export { CombatSystem } from './CombatSystem';
+export type {
+  AbilityActivationRequest,
+  AbilityActivationResult,
+  CombatAbilityAction,
+  CombatSemanticEvent,
+  CombatStatusEffect,
+  CombatantState,
+  CoverDamagePort,
+  DamageResult,
+  DirectHitRequest,
+  ProjectileSpawnSpec,
+  SplashDamageResult,
+  SplashTarget,
+  WeaponFireRequest,
+  WeaponFireResult,
+} from './types';
