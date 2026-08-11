e438ca13823bba4fb3c9ef6dbfa9127bf76553a0
Magyarmex <magyarmex.github@gmail.com>
2026-08-11 14:06:57 -0600

    Mission 20: export canonical lobby scene

---
+++ src/scenes/lobby/index.ts
@@ -0,0 +1,6 @@
+export { LobbyPerformancePolicy, DEFAULT_LOBBY_PERFORMANCE_POLICY } from './LobbyPerformancePolicy';
+export type { LobbyPerformancePolicyConfig } from './LobbyPerformancePolicy';
+export { LobbyBattle } from './LobbyBattle';
+export type { LobbyBattleOptions, LobbyBattleSnapshot } from './LobbyBattle';
+export { LobbyScene } from './LobbyScene';
+export type { LobbySceneFrame } from './LobbyScene';
