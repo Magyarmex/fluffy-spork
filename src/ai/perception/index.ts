dfa31d1c9ac0c48ffbd4eacff3422a0ad5c83039
Magyarmex <magyarmex.github@gmail.com>
2026-08-11 06:05:28 -0600

    Mission 14: publish AI perception boundary

---
+++ src/ai/perception/index.ts
@@ -0,0 +1,10 @@
+export { AIKnowledge } from './AIKnowledge';
+export {
+  DEFAULT_AI_KNOWLEDGE_POLICY,
+  type AIKnowledgeFrame,
+  type AIKnowledgePolicy,
+  type AIObservation,
+  type AwarenessFreshness,
+  type RangeAwareness,
+  type ThreatAwareness,
+} from './types';
